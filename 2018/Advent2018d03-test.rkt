b1d71c7556e45b8b134f8daf1d604645d609edba
mark on github <markongithub@users.noreply.github.com>
2018-12-03 10:53:19 +0000

    make the claim ID part of the claim

--- 2018/Advent2018d03-test.rkt
+++ 2018/Advent2018d03-test.rkt
@@ -2,10 +2,10 @@
  
 (require rackunit "Advent2018d03.rkt")
 
-(check-equal? 4 (countConflicts (list (list 1 3 4 4)
-                                      (list 3 1 4 4)
-                                      (list 5 5 2 2))))
+(check-equal? 4 (countConflicts (list (list 1 1 3 4 4)
+                                      (list 2 3 1 4 4)
+                                      (list 3 5 5 2 2))))
 (check-equal? 4 (countConflicts (map parseClaim (list "#1 @ 1,3: 4x4"
                                                       "#2 @ 3,1: 4x4"
                                                       "#3 @ 5,5: 2x2"))))
-(check-equal? 4 (solvePart1File "input/Advent2018d03.txt"))
+(check-equal? 120419 (solvePart1File "input/Advent2018d03.txt"))
