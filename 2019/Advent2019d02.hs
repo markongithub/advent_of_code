541172ab5f52bc07f9451ccde8ea61239b7844a0
mark on github <markongithub@users.noreply.github.com>
2019-12-12 06:04:00 -0500

    Move the computer into its own module

--- 2019/Advent2019d02.hs
+++ 2019/Advent2019d02.hs
@@ -1,35 +1,8 @@
 module Main where
 
-import Data.Map (Map, (!))
+import IntCode
+import Data.Map ((!))
 import qualified Data.Map as Map
--- import Debug.Trace (trace)
-
-data IntCodeState = IntCodeState { memoryMap :: Map Int Int
-                                 , current :: Int }
-
-processNextInstruction :: IntCodeState -> IntCodeState
-processNextInstruction state = let
-  IntCodeState m c = state
-  -- nextInstruction = trace (show $ Map.elems m) m!c -- this should not fail in these problems
-  nextInstruction = m!c -- this should not fail in these problems
-  in case nextInstruction of
-       99 -> state
-       1  -> processMathInstruction state (+)
-       2  -> processMathInstruction state (*)
-       _  -> error "Unexpected instruction code"
-
-processMathInstruction :: IntCodeState -> (Int -> Int -> Int) -> IntCodeState
-processMathInstruction (IntCodeState m c) op = let
-  result = (m!(m!(c+1))) `op` (m!(m!(c+2)))
-  newLocation = m!(c+3)
-  newMemory = Map.insert newLocation result m
-  nextInstruction = c+4
-  in processNextInstruction (IntCodeState newMemory nextInstruction)
-
-initialStateFromList :: [Int] -> IntCodeState
-initialStateFromList l = let
-  tuples = zip [0..] l
-  in IntCodeState (Map.fromList tuples) 0
 
 runP1Example :: [Int] -> [Int]
 runP1Example l = let
