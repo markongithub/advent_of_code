d8fdfd69b7d8bdaa720f6e0e47525530e3adf745
mark on github <markongithub@users.noreply.github.com>
2020-12-06 13:56:09 -0500

    last commit before hunit

--- 2020/aoc2020/test/Spec.hs
+++ 2020/aoc2020/test/Spec.hs
@@ -4,12 +4,11 @@ import Day01
 prop_sumDigits :: NonNegative Int -> Bool
 prop_sumDigits (NonNegative i)
   | i < 10 = sumDigits str == i
-  | i == 10 = sumDigits str == 1
-  | i == 11 = sumDigits str == 2
+  | i >= 10 && i < 100 = sumDigits str == (i `div` 10 + i `mod` 10)
   | otherwise = True
   where str = show i
 
 main :: IO ()
 main = do
-  quickCheck prop_sumDigits
+  verboseCheck prop_sumDigits
   putStrLn "Done"
