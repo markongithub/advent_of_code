1467412dc104d5c7eaae0d2d9e62b58a093b523b
mark on github <markongithub@users.noreply.github.com>
2020-12-10 18:06:50 -0500

    actually do the problem

--- 2020/aoc2020/src/Day01.hs
+++ 2020/aoc2020/src/Day01.hs
@@ -2,8 +2,18 @@ module Day01 where
 
 import Data.Char (digitToInt, isDigit)
 
-allDigits :: String -> Bool
-allDigits str = all isDigit str
+allPairs :: [a] -> [(a,a)]
+allPairs [] = []
+allPairs [x] = []
+allPairs (x:xs) = let
+  withX = zip (repeat x) xs
+  withoutX = allPairs xs
+  in withX ++ withoutX
 
-sumDigits :: String -> Int
-sumDigits str = sum $ map digitToInt str
+sumsTo2020 :: (Int,Int) -> Bool
+sumsTo2020 (x,y) = x + y == 2020
+
+productOf2020Sums :: [Int] -> Int
+productOf2020Sums xs = let
+  (x,y) = head $ filter sumsTo2020 $ allPairs xs
+  in x * y
