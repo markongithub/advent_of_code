2de9f264d619c92b3cd84126899c1a473b9edec6
mark on github <markongithub@users.noreply.github.com>
2020-12-13 09:35:38 -0500

    wow this all worked on the first try

--- 2020/aoc2020/test/Day12Test.hs
+++ 2020/aoc2020/test/Day12Test.hs
@@ -3,17 +3,12 @@ import Test.Tasty.HUnit (assertEqual, testCase)
 
 import Day12
 
-day12TestData = lines "F10\
-\N3\
-\F7\
-\R90\
-\F11"
+day12TestData = ["F10", "N3", "F7", "R90", "F11"]
 
 day12Test = testCase []
-  (assertEqual [] 0 0)
+  (assertEqual [] 25 (solvePart1Pure day12TestData))
 
 main = do
---  day12Solution1 <- solvePart1
---  let day12SolutionTest = testCase [] (assertEqual [] 55776 day12Solution1)
-  defaultMain $ testGroup []
-    [day12Test]
+  day12Solution1 <- solvePart1
+  let day12Solution1Test = testCase [] (assertEqual [] 796 day12Solution1)
+  defaultMain $ testGroup [] [day12Test, day12Solution1Test]
