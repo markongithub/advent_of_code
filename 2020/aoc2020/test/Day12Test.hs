504417c9e37fb446e3ca03c1f62406701b6a8eca
mark on github <markongithub@users.noreply.github.com>
2020-12-13 09:56:52 -0500

    solve part 2

--- 2020/aoc2020/test/Day12Test.hs
+++ 2020/aoc2020/test/Day12Test.hs
@@ -8,7 +8,13 @@ day12TestData = ["F10", "N3", "F7", "R90", "F11"]
 day12Test = testCase []
   (assertEqual [] 25 (solvePart1Pure day12TestData))
 
+day12Test2 = testCase []
+  (assertEqual [] 286 (solvePart2Pure day12TestData))
+
 main = do
   day12Solution1 <- solvePart1
   let day12Solution1Test = testCase [] (assertEqual [] 796 day12Solution1)
-  defaultMain $ testGroup [] [day12Test, day12Solution1Test]
+  day12Solution2 <- solvePart2
+  let day12Solution2Test = testCase [] (assertEqual [] 39446 day12Solution2)
+  defaultMain $ testGroup []
+    [day12Test, day12Solution1Test, day12Test2, day12Solution2Test]
