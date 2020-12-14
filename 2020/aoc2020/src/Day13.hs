f54433fae664f03261aa1ef4e9407eb9b1c2dd43
mark on github <markongithub@users.noreply.github.com>
2020-12-13 19:27:53 -0500

    commit before I break things, clean up later

--- 2020/aoc2020/src/Day13.hs
+++ 2020/aoc2020/src/Day13.hs
@@ -1,6 +1,6 @@
 module Day13 where
 
-import Data.List (minimumBy)
+import Data.List (minimumBy, sortBy)
 
 waitForBus :: Int -> Int -> Int
 waitForBus timestamp bus = let
@@ -56,3 +56,65 @@ solvePart1 = do
   text <- readFile "data/input13.txt"
   let [timestampStr, busLine] = lines text
   return $ solvePart1Pure timestampStr busLine
+
+sortByFstDesc :: (Ord a) => [(a, b)] -> [(a, b)]
+sortByFstDesc ls = sortBy (\(d1, _) (d2, _) -> compare d2 d1) ls
+
+checkTimestampForBus :: Int -> (Int, Int) -> Bool
+checkTimestampForBus timestamp (busID, offset) =
+  timestamp `mod` busID == ((busID - offset) `mod` busID)
+
+checkTimestampForAllBuses :: [(Int, Int)] -> Int -> Bool
+checkTimestampForAllBuses buses timestamp =
+  all (checkTimestampForBus timestamp) buses
+
+candidateTimestamps :: (Int, Int) -> [Int]
+candidateTimestamps (busID, offset) = map (\k -> (busID - offset) + (busID * k)) [0..]
+
+busToSaneOffset :: (Int, Int) -> (Int, Int)
+busToSaneOffset (busID, offset) = let
+  offset2 = offset `mod` busID
+  offset3 = case offset2 of
+    0 -> 0
+    _ -> busID - offset2
+  in (busID, offset3)
+
+-- lists must be ordered
+leastCommonElement :: (Ord a) => [a] -> [a] -> a
+leastCommonElement (x:xs) (y:ys)
+  | x == y    = x
+  | x > y     = leastCommonElement (x:xs) (dropWhile (< x) (y:ys))
+  | otherwise = leastCommonElement (dropWhile (< y) xs) (y:ys)
+
+mergeTwoOffsets :: (Int, Int) -> (Int, Int) -> (Int, Int)
+mergeTwoOffsets (a, x) (b, y) = let
+  candidates = map (\k -> (k*a) + x) [0..b]
+  selector c = c `mod` b == y
+  newOffset = head $ filter selector candidates
+  in (a * b, newOffset)
+
+mergeOffsets :: [(Int, Int)] -> (Int, Int)
+mergeOffsets xs = foldl mergeTwoOffsets (1, 0) xs
+
+earliestCorrectTimestamp :: [(Int, Int)] -> Int
+earliestCorrectTimestamp buses = let
+  saneOffsets = map busToSaneOffset (sortByFstDesc buses)
+  in snd $ mergeOffsets saneOffsets
+  
+--earliestCorrectTimestamp buses = let
+--  sortedBuses = sortByFstDesc buses
+--  maxBus = head sortedBuses
+--  otherBuses = tail sortedBuses
+--  correctTimestamps = filter (checkTimestampForAllBuses otherBuses) (candidateTimestamps maxBus)
+--  in head correctTimestamps
+
+solvePart2Pure :: String -> Int
+solvePart2Pure busLine = earliestCorrectTimestamp $ parseBusLine busLine
+
+solvePart2 :: IO Int
+solvePart2 = do
+  text <- readFile "data/input13.txt"
+  let busLine = (lines text)!!1
+  putStrLn (show $ sortByFstDesc $ parseBusLine busLine)
+  return $ solvePart2Pure busLine
+
--- 2020/aoc2020/test/Day13Test.hs
+++ 2020/aoc2020/test/Day13Test.hs
@@ -4,15 +4,50 @@ import Test.Tasty.HUnit (assertEqual, testCase)
 import Day13
 
 day13TestTimestamp = "939"
-day13TestBusLine = "7,13,x,x,59,x,31,19"
+day13TestBusLine1 = "7,13,x,x,59,x,31,19"
 
 day13Test1 = testCase []
-  (assertEqual [] 295 (solvePart1Pure day13TestTimestamp day13TestBusLine))
+  (assertEqual [] 295 (solvePart1Pure day13TestTimestamp day13TestBusLine1))
+
+testPart2 (input, expected) = testCase [] (assertEqual [] expected (solvePart2Pure input))
+
+buses = parseBusLine "7,x,13"
+sortedBuses = sortByFstDesc buses
+maxBus = head sortedBuses
+otherBuses = tail sortedBuses
+candidates = candidateTimestamps maxBus
+correctTimestamps = filter (checkTimestampForAllBuses otherBuses) candidates
+
+testOffsets = map busToSaneOffset [(661,44),(641,13),(41,3),(37,50),(29,42),(23,67),(19,25),(17,30),(13,0)]
+
+purePart2Tests = [
+    testCase [] (assertEqual [] [7,14,21] (take 3 $ candidateTimestamps (7,0)))
+  , testCase [] (assertEqual [] [11,24,37,50,63] (take 5 $ candidates))
+  , testCase [] (assertEqual [] True (checkTimestampForBus 38 (13, 1)))
+  , testCase [] (assertEqual [] False (checkTimestampForBus 40 (13, 1)))
+  , testCase [] (assertEqual [] [(13,2),(7,0)] (parseBusLine "7,x,13"))
+  , testCase [] (assertEqual [] (13,2) maxBus)
+  , testCase [] (assertEqual [] [(7,0)] otherBuses)
+  , testCase [] (assertEqual [] True (checkTimestampForBus 63 (7,0)))
+  , testCase [] (assertEqual [] True (checkTimestampForAllBuses otherBuses 63))
+  , testCase [] (assertEqual [] [False,False,False,False,True] (map (checkTimestampForAllBuses otherBuses) (take 5 candidates)))
+  , testCase [] (assertEqual [] (13,2) (head $ sortByFstDesc $ parseBusLine "7,x,13"))
+  , testCase [] (assertEqual [] 63 (earliestCorrectTimestamp $ parseBusLine "7,x,13"))
+  , testCase [] (assertEqual [] (91,76) (mergeTwoOffsets (13,11) (7,6)))
+  , testPart2 ("7,13", 77)
+  , testPart2 ("7,x,13", 63)
+  , testPart2 (day13TestBusLine1, 1068781)
+  , testPart2 ("67,7,59,61", 754018)
+  , testPart2 ("67,x,7,59,61", 779210)
+  , testPart2 ("67,7,x,59,61", 1261476)
+  , testPart2 ("1789,37,47,1889", 1202161486)
+  , testCase [] (assertEqual [] (37,24) (busToSaneOffset (37,50)))
+  ]
 
 main = do
   day13Solution1 <- solvePart1
   let day13Solution1Test = testCase [] (assertEqual [] 3966 day13Solution1)
---  day13Solution2 <- solvePart2
---  let day13Solution2Test = testCase [] (assertEqual [] 39446 day13Solution2)
+  day13Solution2 <- solvePart2
+  let day13Solution2Test = testCase [] (assertEqual [] 800177252346225 day13Solution2)
   defaultMain $ testGroup []
-    [day13Test1, day13Solution1Test] -- , day13Test2, day13Solution2Test]
+    ([day13Test1, day13Solution1Test] ++ purePart2Tests ++ [day13Solution2Test])
