ed42bf63c64f37d7488eb7f9cc9357ab78200a48
mark on github <markongithub@users.noreply.github.com>
2020-12-15 13:22:01 +0000

    screw this. but it's solved.

--- 2020/aoc2020/src/Day15.hs
+++ 2020/aoc2020/src/Day15.hs
@@ -2,36 +2,39 @@ module Day15 where
 
 import Common
 import Data.Map (Map)
-import qualified Data.Map as Map
+import qualified Data.Map.Strict as Map
 import Data.Char (digitToInt)
 import Debug.Trace (trace)
 
 data GameState = GameState { startingNumbersAcc :: [Int]
-                           , nextSpokenAcc :: Int
+                           , nextAgeAcc :: Int
                            , currentTurnAcc :: Int
                            , memoryAcc :: Map Int Int
                            } deriving (Eq, Show)
 
-speakNextNumber :: GameState -> (Int, GameState)
-speakNextNumber (GameState starting next currentTurn memory) = let
-  debugStr = ("turn " ++ show currentTurn ++ " map size " ++ show (Map.size memory))
-  nextTurn = (currentTurn + 1) -- if (currentTurn `mod` 10000 == 0) then (trace debugStr (currentTurn + 1)) else (currentTurn + 1)
+advanceGame :: GameState -> GameState
+advanceGame (GameState starting next currentTurn memory) = let
+  debugStr = ("turn " ++ show currentTurn ++ " speaking " ++ show next) --  ++ " map max " ++ show (maximum $ Map.elems memory))
+  nextTurn = if (currentTurn `mod` 1000000 == 0) then (trace debugStr (currentTurn + 1)) else (currentTurn + 1)
   (spokenNumber, nextStarting) = case starting of
     []     -> (next, [])
     (x:xs) -> (x,xs)
-  nextSpoken = case (Map.lookup spokenNumber memory) of
+  nextAge = case (Map.lookup spokenNumber memory) of
     Nothing -> 0
     Just t  -> (currentTurn - t)
   newMemory = Map.insert spokenNumber currentTurn memory
-  nextState = GameState nextStarting nextSpoken nextTurn newMemory
-  in (spokenNumber, nextState)
+  nextState = GameState nextStarting nextAge nextTurn newMemory
+  in nextState
 
-silentTurn :: GameState -> GameState
-silentTurn state = snd $ speakNextNumber state
-    
+nextSpoken :: GameState -> Int
+nextSpoken (GameState starting age _ _) = case starting of
+  []     -> age
+  (x:xs) -> x
+
 spokenNumbers :: GameState -> [Int]
 spokenNumbers old = let
-  (next, new) = speakNextNumber old
+  next = nextSpoken old
+  new = advanceGame old
   in (next:(spokenNumbers new))
 
 initialState :: [Int] -> GameState
@@ -48,15 +51,22 @@ spokenNumbersFromInput input = let
   startingNumbers = parseInput input
   in spokenNumbers (initialState startingNumbers)
 
+iterateQuiet :: Int -> (GameState -> GameState) -> GameState -> GameState
+iterateQuiet 0 _ x = x
+iterateQuiet n f x = let
+  output = iterateQuiet (n - 1) f $! (f x)
+  shouldTrace = n `mod` 1000000 == 0
+  in if shouldTrace then (trace ("iterateQuiet: " ++ show (n, Map.size $ memoryAcc x)) output) else output
+
 stateAfterNWords :: Int -> String -> GameState
 stateAfterNWords n input = let
   startingNumbers = parseInput input
-  in head $ drop (n-1) $ iterate silentTurn (initialState startingNumbers)
+  in iterateQuiet n advanceGame (initialState startingNumbers)
 
 nthWord :: Int -> String -> Int
 nthWord n input = let
-  finalState = stateAfterNWords n input
-  in nextSpokenAcc $ finalState
+  finalState = stateAfterNWords (n-1) input
+  in nextSpoken $ finalState
 
 solvePart1Func :: String -> Int
 solvePart1Func input = nthWord 2020 input
@@ -65,5 +75,6 @@ day15Input = "1,2,16,19,18,0"
 solvePart1 :: Int
 solvePart1 = solvePart1Func day15Input
 
-solvePart2Func input = nthWord 30000000 input
+part2Number = 30000000
+solvePart2Func input = nthWord part2Number input
 solvePart2 = solvePart2Func day15Input
--- 2020/aoc2020/test/Day15Test.hs
+++ 2020/aoc2020/test/Day15Test.hs
@@ -9,10 +9,11 @@ makeTest expected actual = testCase [] (assertEqual [] expected actual)
 day15PureTests = [ makeTest 436 (solvePart1Func day15TestData1)
                  , makeTest [0,3,6,0,3,3,1,0,4,0] (take 10 $ spokenNumbersFromInput day15TestData1)
                  , makeTest 4 (nthWord 9 day15TestData1)
+                 , makeTest [0,3,3,1,0,4,0] (map (\x -> nthWord x day15TestData1) [4..10])
                  , makeTest 536 solvePart1
-                 , makeTest 175594 (solvePart2Func day15TestData1)
-                 , makeTest 2578 (solvePart2Func "1,3,2")
---                 , makeTest 536 solvePart2
+--                 , makeTest 175594 (solvePart2Func day15TestData1)
+--                 , makeTest 2578 (solvePart2Func "1,3,2")
+                 , makeTest 24065124 solvePart2
                  ]
 
 main = defaultMain $ testGroup [] day15PureTests
