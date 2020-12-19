28f62012fea3f9574b0a7a63f983400fb49d8628
mark on github <markongithub@users.noreply.github.com>
2020-12-19 15:24:38 -0500

    parsing seems to work

--- 2020/aoc2020/src/Day19.hs
+++ 2020/aoc2020/src/Day19.hs
@@ -5,9 +5,9 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 
 data TempRule = RealRule Rule | TempDisjunction TempRule TempRule
-              | TempConcatenation [Int] deriving (Eq, Show)
+              | TempConcat [Int] deriving (Eq, Show)
 
-data Rule = StringMatch String | Disjunction Rule Rule | Concatenation Rule Rule
+data Rule = StringMatch String | Disjunction Rule Rule | Concat [Rule]
             deriving (Eq, Show)
 
 parseRule :: String -> (Int, TempRule)
@@ -18,8 +18,8 @@ parseRule s = let
   rule = case restOfRule of
     ('"':xs) -> RealRule $ StringMatch (init xs)
     other -> case (parseDisjunction other) of
-      Nothing -> parseConcatenation other
-      Just (a, b) -> TempDisjunction (parseConcatenation a) (parseConcatenation b)
+      Nothing -> parseConcat other
+      Just (a, b) -> TempDisjunction (parseConcat a) (parseConcat b)
   in (ruleID, rule)
 
 parseDisjunction :: String -> Maybe (String, String)
@@ -29,9 +29,40 @@ parseDisjunction s = let
     [] -> Nothing
     fromPipeStr -> Just (init (takeWhile (/= '|') s), tail fromPipeStr)
 
-parseConcatenation :: String -> TempRule
-parseConcatenation str = let
+parseConcat :: String -> TempRule
+parseConcat str = let
   substrings = splitOnSeparator ' ' str
   ints = map (\s -> read s :: Int) substrings
-  in TempConcatenation ints
+  in TempConcat ints
+
+parseRules0 :: [String] -> Map Int TempRule -> Map Int TempRule
+parseRules0 [] oldMap = oldMap
+parseRules0 (x:xs) oldMap = let
+  (ruleID, rule) = parseRule x
+  newMap = Map.insert ruleID rule oldMap
+  in parseRules0 xs newMap
+
+parseRules :: [String] -> Map Int TempRule
+parseRules ls = parseRules0 ls Map.empty
+
+flattenRuleByNumber :: Map Int TempRule -> Int -> Rule
+flattenRuleByNumber m i = case (Map.lookup i m) of
+  Nothing -> error "rule not found in table"
+  Just r -> flattenTempRule m r
+
+flattenTempRule :: Map Int TempRule -> TempRule -> Rule
+flattenTempRule _ (RealRule r) = r
+flattenTempRule m (TempDisjunction a b) = Disjunction (flattenTempRule m a) (flattenTempRule m b)
+flattenTempRule m (TempConcat ls) = flattenConcat $ Concat (map (flattenRuleByNumber m) ls)
+
+flattenConcat :: Rule -> Rule
+flattenConcat (StringMatch s) = StringMatch s
+flattenConcat (Disjunction a b) = Disjunction a b
+flattenConcat (Concat []) = StringMatch ""
+flattenConcat (Concat ls) = let
+  newList = map flattenConcat ls
+  in case newList of
+    [x] -> x
+    (StringMatch s1:StringMatch s2:ys) -> flattenConcat $ Concat (StringMatch (s1 ++ s2):ys)
+    ls -> Concat ls
 
--- 2020/aoc2020/test/Day19Test.hs
+++ 2020/aoc2020/test/Day19Test.hs
@@ -3,10 +3,16 @@ import Test.Tasty.HUnit (assertEqual, testCase)
 
 import Day19
 
+day19TestRules = ["0: 4 1 5", "1: 2 3 | 3 2", "2: 4 4 | 5 5", "3: 4 5 | 5 4","4: \"a\"","5: \"b\""]
+day19TestRulesParsed = Concat [StringMatch "a",Disjunction (Concat [Disjunction (StringMatch "aa") (StringMatch "bb"),Disjunction (StringMatch "ab") (StringMatch "ba")]) (Concat [Disjunction (StringMatch "ab") (StringMatch "ba"),Disjunction (StringMatch "aa") (StringMatch "bb")]),StringMatch "b"]
+
 makeTest expected actual = testCase [] (assertEqual [] expected actual)
-day19PureTests = [ makeTest (5, TempConcatenation [72, 58]) (parseRule "5: 72 58")
-                 , makeTest (128, TempDisjunction (TempConcatenation [66, 58]) (TempConcatenation [57, 72])) (parseRule "128: 66 58 | 57 72")
+day19PureTests = [ makeTest (5, TempConcat [72, 58]) (parseRule "5: 72 58")
+                 , makeTest (128, TempDisjunction (TempConcat [66, 58]) (TempConcat [57, 72])) (parseRule "128: 66 58 | 57 72")
                  , makeTest (58, RealRule (StringMatch "b")) (parseRule "58: \"b\"")
+                 , makeTest day19TestRulesParsed (flattenRuleByNumber (parseRules day19TestRules) 0)
                  ]
+
+
 
 main = defaultMain $ testGroup [] day19PureTests
