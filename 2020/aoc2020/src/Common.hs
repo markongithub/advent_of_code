958f25d9e32d291c071968d4e0f7f07b5661d304
mark on github <markongithub@users.noreply.github.com>
2020-12-19 13:56:01 -0500

    okay this works, commit before I break it

--- 2020/aoc2020/src/Common.hs
+++ 2020/aoc2020/src/Common.hs
@@ -1,15 +1,18 @@
 module Common where
 
 -- plagiarizing from my own 2019d03 solution
-splitOnCommas0 :: String -> [String] -> [String]
-splitOnCommas0 str accu = case str of
-  [] -> reverse accu
-  (',':xs) -> splitOnCommas0 xs accu
-  _ -> let
-    nextSegment = takeWhile (/= ',') str
-    remainder = dropWhile (/= ',') str
+splitOnSeparator0 :: Char -> String -> [String] -> [String]
+splitOnSeparator0 separator str accu
+  | null str = reverse accu
+  | head str == separator = splitOnSeparator0 separator (tail str) accu
+  | otherwise = let
+    nextSegment = takeWhile (/= separator) str
+    remainder = dropWhile (/= separator) str
     newAccu = nextSegment:accu
-    in splitOnCommas0 remainder newAccu
+    in splitOnSeparator0 separator remainder newAccu
+
+splitOnSeparator :: Char -> String -> [String]
+splitOnSeparator separator str = splitOnSeparator0 separator str []
 
 splitOnCommas :: String -> [String]
-splitOnCommas str = splitOnCommas0 str []
+splitOnCommas = splitOnSeparator ','
