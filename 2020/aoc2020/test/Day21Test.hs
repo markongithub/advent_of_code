4ac02f647940a7988ff1affc4584963e63f08490
mark on github <markongithub@users.noreply.github.com>
2020-12-21 20:16:10 -0500

    solve part 1

--- 2020/aoc2020/test/Day21Test.hs
+++ 2020/aoc2020/test/Day21Test.hs
@@ -9,14 +9,19 @@ import Day21
 
 makeTest expected actual = testCase [] (assertEqual [] expected actual)
 
-day21PureTests = [ makeTest (Set.singleton $ Set.fromList ["sqjhc", "fvjkl"]) (day21TestMap!"soy")
-                 , makeTest 2 (length $ day21TestMap!"fish")
-                 , makeTest False (isSolved day21TestMap)
+day21PureTests = [ makeTest (Set.singleton $ Set.fromList ["sqjhc", "fvjkl"]) (day21FullMap!"soy")
+                 , makeTest 2 (length $ day21FullMap!"fish")
+                 , makeTest (Possibilities $ Set.singleton "mxmxvkd") (day21ReducedMap!"dairy")
+                 , makeTest False (isSolved day21ReducedMap)
+                 , makeTest ("dairy", "mxmxvkd") (findSingleIngredient day21ReducedMap)
+                 , makeTest (Possibilities (Set.singleton "sqjhc")) ((solveAllergen day21ReducedMap "dairy" "mxmxvkd")!"fish")
+                 , makeTest (Possibilities (Set.singleton "sqjhc")) ((solveAllergen day21ReducedMap "dairy" "mxmxvkd")!"fish")
+                 , makeTest 5 (solvePart1Func day21TestInput)
                  ]
 
 main = do
-  -- day21Solution1 <- solvePart1 "data/input21.txt"
-  -- let day21Solution1Test = testCase [] (assertEqual [] 142
-  --                                       day21Solution1)
-  defaultMain $ testGroup [] (day21PureTests) --  ++ [
-  --  day21Solution1Test, part2Test1, part2Test2, day21Solution2Test])
+  day21Solution1 <- solvePart1
+  let day21Solution1Test = testCase [] (assertEqual [] 2436
+                                        day21Solution1)
+  defaultMain $ testGroup [] (day21PureTests ++ [
+    day21Solution1Test]) -- , part2Test1, part2Test2, day21Solution2Test])
