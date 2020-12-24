3c6a007791c4ba000f69ce30b4c3859a39fc1c09
mark on github <markongithub@users.noreply.github.com>
2020-12-23 22:22:20 -0500

    solve part 2

--- 2020/aoc2020/src/Day23.hs
+++ 2020/aoc2020/src/Day23.hs
@@ -1,15 +1,14 @@
-module Day23 where
-
-import Data.Foldable (toList)
-import Data.Maybe (isNothing)
-import Data.Sequence (Seq, Seq((:<|)), Seq((:|>)), (><))
-import qualified Data.Sequence as Seq
+import Data.Map.Strict (Map, (!))
+import qualified Data.Map.Strict as Map
 import Debug.Trace (trace)
 
-data GameState = GameState !SmartLL Int Int deriving (Eq, Show)
+data GameState = GameState !Int !(Map Int Int) Int Int deriving (Eq, Show)
 
 fromList :: [Int] -> GameState
-fromList ls = GameState [ls] (minimum ls) (maximum ls)
+fromList ls = let
+  loopPair = (last ls, head ls)
+  allPairs = loopPair:(zip ls (tail ls))
+  in GameState (head ls) (Map.fromList allPairs) (minimum ls) (maximum ls)
 
 findDestination :: Int -> Int -> Int -> [Int] -> Int
 findDestination current minLabel maxLabel forbidden
@@ -17,38 +16,43 @@ findDestination current minLabel maxLabel forbidden
   | all (/= current) forbidden = current
   | otherwise = findDestination (pred current) minLabel maxLabel forbidden
 
+takeM :: Int -> Int -> Map Int Int -> [Int]
+takeM 0 k m = []
+takeM n k m = (m!k):(takeM (n - 1) (m!k) m)
+
+cupsInOrder :: GameState -> [Int]
+cupsInOrder (GameState current cups _ _) = let
+  rest = takeM (Map.size cups - 1) current cups
+  in current:rest
+
 playTurn :: GameState -> GameState
 playTurn g = let
-  GameState cups minLabel maxLabel = g
-  (current, rest) = deconstructLL cups
-  (pickedUp, notPickedUp) = takeLL 3 rest
+  GameState current cups minLabel maxLabel = g
+  pickedUp = takeM 3 current cups
   destination = findDestination (pred current) minLabel maxLabel pickedUp
-  (cupsBeforeDest, cupsAfterDest) = splitLL (== destination) notPickedUp
-  -- finalCups = (cupsBeforeDest:((destination:pickedUp):cupsAfterDest)) ++ [[current]]
-  finalCups = (cupsBeforeDest:((current:(destination:pickedUp)):cupsAfterDest))
-  in GameState finalCups minLabel maxLabel
+  oldCurrentNext = head pickedUp
+  oldDestinationNext = cups!destination
+  oldPickedUpNext = cups!(last pickedUp)
+  newPairs = [(current, oldPickedUpNext), (destination, oldCurrentNext), (last pickedUp, oldDestinationNext)]
+  newPairMap = Map.fromList newPairs
+  newMap = Map.union newPairMap cups
+  newCurrent = oldPickedUpNext
+  in GameState newCurrent newMap minLabel maxLabel
 
-playTurnAndFlatten :: GameState -> GameState
-playTurnAndFlatten (GameState cups minLabel maxLabel) = let
-  flattened = flattenLL cups
-  newCups = trace ("maximum cup label is " ++ show (maximum flattened)) [flattenLL cups]
-  in seq newCups $ playTurn (GameState newCups minLabel maxLabel)
-
-strictMod = 10
+strictMod = 20000
 playNTurns :: GameState -> Int -> GameState
 playNTurns state n
   | n == 0 = state
-  | (n `mod` strictMod == 0) && trace ("LL is of length " ++ (show $ length cups) ++ " with " ++ show n ++ " turns left.") False = undefined
+  | (n `mod` strictMod == 0) && trace ("current cup is " ++ (show $ current) ++ " with " ++ show n ++ " turns left.") False = undefined
   | otherwise = seq nextState $ playNTurns nextState (n-1)
   where nextFunc = playTurn -- if (n `mod` strictMod == 0) then playTurnAndFlatten else playTurn
         nextState = nextFunc $! state
-        GameState cups minLabel maxLabel = state
+        GameState current cups minLabel maxLabel = state
 
 labelsAfter1 :: GameState -> String
-labelsAfter1 (GameState cups _ _) = let
-  (cupsBefore1, cupsAfter1) = splitLL (== 1) cups
-  finalCups = cupsAfter1 ++ [cupsBefore1]
-  in concat (map show $ flattenLL finalCups)
+labelsAfter1 (GameState _ cups _ _) = let
+  ints = takeM (Map.size cups - 1) 1 cups
+  in concat (map show $ ints)
 
 solvePart1Func :: GameState -> String
 solvePart1Func g = labelsAfter1 $ playNTurns g 100
@@ -65,13 +69,8 @@ initialLongList cups = let
   in cups ++ additionalCups
 
 twoCupsAfter1 :: GameState -> (Int, Int)
-twoCupsAfter1 (GameState cups _ _) = let
-  (cupsBefore1, cupsAfter1) = splitLL (== 1) cups
-  twoFromAfter = fst $ safeTakeLL 2 cupsAfter1
-  numCupsAfter1 = length twoFromAfter
-  twoCups = if (numCupsAfter1 == 2) then twoFromAfter
-              else (twoFromAfter ++ (take (2 - numCupsAfter1) cupsBefore1))
-  finalList = twoCups
+twoCupsAfter1 (GameState _ cups _ _) = let
+  finalList = takeM 2 1 cups
   in (finalList!!0, finalList!!1)
 
 part2Product :: GameState -> Int
@@ -84,59 +83,3 @@ solvePart2 = let
   initialState = fromList (initialLongList part1Input)
   finalState = playNTurns initialState 10000000
   in part2Product finalState
-
-current :: GameState -> Int
-current (GameState ll _ _) = headLL ll
-
-type SmartLL = [[Int]]
-
-headLL :: SmartLL -> Int
-headLL [] = error "headLL on empty LL"
-headLL ([]:xs) = headLL xs
-headLL ((x:xs):ys) = x
-
--- this should be O(1)
-deconstructLL :: SmartLL -> (Int, SmartLL)
-deconstructLL [] = error "deconstructLL on empty LL"
-deconstructLL ([]:xs) = deconstructLL xs
-deconstructLL ((x:xs):ys) = (x, (xs:ys))
-
--- this should be O(n) in the n you're taking
-takeLL0 :: Int -> SmartLL -> [Int] -> ([Int], SmartLL)
-takeLL0 0 xs accu = (reverse accu, xs)
-takeLL0 n [] _ = error "ran out of list"
-takeLL0 n ([]:xs) accu = takeLL0 n xs accu
-takeLL0 n ((x:xs):ys) accu = takeLL0 (pred n) (xs:ys) (x:accu)
-
-takeLL :: Int -> SmartLL -> ([Int], SmartLL)
-takeLL n xs = takeLL0 n xs []
-
-safeTakeLL :: Int -> SmartLL -> ([Int], SmartLL)
-safeTakeLL 0 xs = ([], xs)
-safeTakeLL n [] = ([], [])
-safeTakeLL n ([]:xs) = safeTakeLL n xs
-safeTakeLL n ((x:xs):ys) = let
-  (a, b) = safeTakeLL (pred n) (xs:ys)
-  in (x:a, b)
-
-splitLL0 :: (Int -> Bool) -> SmartLL -> [Int] -> ([Int], SmartLL)
-splitLL0 p [] _ = error "can't split an empty list"
-splitLL0 p ([]:xs) accu = splitLL0 p xs accu
-splitLL0 p ((x:xs):ys) accu = let
-  allTheRest = (xs:ys)
-  done = (reverse accu, allTheRest)
-  continue = splitLL0 p allTheRest (x:accu)
-  in if (p x) then done else continue
-
-splitLL :: (Int -> Bool) -> SmartLL -> ([Int], SmartLL)
-splitLL p xs = splitLL0 p xs []
-
-flattenLL0 :: SmartLL -> [Int] -> [Int]
-flattenLL0 [] accu = reverse accu
-flattenLL0 ([]:xs) accu = flattenLL0 xs accu
-flattenLL0 ((x:xs):ys) accu = flattenLL0 (xs:ys) (x:accu)
-
-flattenLL :: SmartLL -> [Int]
-flattenLL [] = []
-flattenLL ([]:xs) = flattenLL xs
-flattenLL ((x:xs):ys) = x:(flattenLL (xs:ys))
--- 2020/aoc2020/test/Day23Test.hs
+++ 2020/aoc2020/test/Day23Test.hs
@@ -1,19 +1,21 @@
 import Test.Tasty (defaultMain, testGroup, TestTree)
 import Test.Tasty.HUnit (assertEqual, testCase)
-import qualified Data.Sequence as Seq
+import qualified Data.Map as Map
 
 import Day23
 
 makeTest expected actual = testCase [] (assertEqual [] expected actual)
 
-sameCups expected (GameState cups _ _)  = testCase [] (assertEqual [] expected (flattenLL cups))
+sameCups expected state  = testCase [] (assertEqual [] expected (cupsInOrder state))
 
+initialTestState :: GameState
 initialTestState = fromList [3,8,9,1,2,5,4,6,7]
 longTestState = fromList $ initialLongList [3,8,9,1,2,5,4,6,7]
 
 day23PureTests = [
-    sameCups [2,8,9,1,5,4,6,7,3] (playTurn initialTestState)
-  , makeTest (GameState [[3,8,9,1,2,5,4,6,7]] 1 9) initialTestState
+    sameCups [3,8,9,1,2,5,4,6,7] initialTestState
+  , sameCups [2,8,9,1,5,4,6,7,3] (playTurn initialTestState)
+  , makeTest (GameState 3 (Map.fromList [(1,2),(2,5),(3,8),(4,6),(5,4),(6,7),(7,3),(8,9),(9,1)]) 1 9) initialTestState
   , makeTest "54673289" (labelsAfter1 $ fromList [2,8,9,1,5,4,6,7,3])
   , sameCups [8,3,7,4,1,9,2,6,5] (playNTurns initialTestState 10)
   , makeTest "74698532" solvePart1
@@ -23,18 +25,8 @@ day23PureTests = [
   , makeTest 10 (part2Product $ fromList [5,4,6,7,3,8,9,1,2])
   , makeTest (5,4) (twoCupsAfter1 (playTurn longTestState))
   , makeTest (3,2) (twoCupsAfter1 (playNTurns longTestState 2))
-  , makeTest 5 (current (playNTurns longTestState 500))
---  , makeTest 5556969 solvePart2
+  , makeTest 286194102744 solvePart2
   ]
 
 main = do
---  day23Solution1 <- solvePart1
---  let day23Solution1Test = testCase [] (assertEqual [] 2436
---                                        day23Solution1)
---  day23Solution2 <- solvePart2
---  let day23Solution2Test =
---        testCase [] (
---          assertEqual [] "dhfng,pgblcd,xhkdc,ghlzj,dstct,nqbnmzx,ntggc,znrzgs"
---            day23Solution2)
-  defaultMain $ testGroup [] (day23PureTests) --  ++ [
---    day23Solution1Test, day23Solution2Test])
+  defaultMain $ testGroup [] day23PureTests
