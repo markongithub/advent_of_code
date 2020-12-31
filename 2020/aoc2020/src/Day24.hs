528b03cc90917bfbab82bf980a1a318982a48f9f
mark on github <markongithub@users.noreply.github.com>
2020-12-30 20:47:34 -0500

    finish part 2

--- 2020/aoc2020/src/Day24.hs
+++ 2020/aoc2020/src/Day24.hs
@@ -5,7 +5,8 @@ import qualified Data.Map as Map
 
 data Color = White | Black deriving (Eq, Show)
 
-type Tiles = Map (Int, Int) Color
+type Location = (Int, Int)
+type Tiles = Map Location Color
 
 addPairs :: (Num a, Num b) => (a,b) -> (a,b) -> (a,b)
 addPairs (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
@@ -13,7 +14,7 @@ addPairs (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 sumPairs :: (Num a, Num b) => [(a,b)] -> (a,b)
 sumPairs xs = foldl addPairs (0,0) xs
 
-parseDirections :: String -> [(Int, Int)]
+parseDirections :: String -> [Location]
 parseDirections [] = []
 parseDirections (x:xs) = let
   parseXS = parseDirections xs
@@ -33,7 +34,7 @@ flipColor :: Color -> Color
 flipColor Black = White
 flipColor White = Black
 
-flipTile :: Tiles -> (Int, Int) -> Tiles
+flipTile :: Tiles -> Location -> Tiles
 flipTile m k = let
   newColor = case Map.lookup k m of
     Nothing -> Black
@@ -41,7 +42,7 @@ flipTile m k = let
   in Map.insert k newColor m
 
 -- I can't use Map.union for this because the order of updating matters
-flipTiles :: Tiles -> [(Int, Int)] -> Tiles
+flipTiles :: Tiles -> [Location] -> Tiles
 flipTiles m tiles = foldl flipTile m tiles
 
 countBlackTiles :: Tiles -> Int
@@ -56,3 +57,57 @@ solvePart1 :: IO Int
 solvePart1 = let
   input = readFile "data/input24.txt"
   in fmap (solvePart1Func . lines) input
+
+adjacentOffsets :: [Location]
+adjacentOffsets = map (head . parseDirections) ["e", "se", "sw", "w", "nw", "ne"]
+
+adjacentsOfTile :: Location -> [Location]
+adjacentsOfTile tile = map (addPairs tile) adjacentOffsets
+
+addImplicitAdjacents :: Tiles -> Location -> Tiles
+addImplicitAdjacents m location = let
+  adjacents = adjacentsOfTile location
+  makePair t = (t, Map.findWithDefault White t m)
+  in Map.union m (Map.fromList $ map makePair adjacents)
+
+addAllImplicitAdjacents :: Tiles -> Tiles
+addAllImplicitAdjacents m = let
+  blackLocations = map fst $ filter (\p -> snd p == Black) $ Map.toList m
+  in foldl addImplicitAdjacents m blackLocations
+
+countBlackAdjacents :: Tiles -> Location -> Int
+countBlackAdjacents m t = let
+  adjacents = adjacentsOfTile t
+  colors = map (\t -> Map.findWithDefault White t m) adjacents
+  in length $ filter (== Black) colors
+
+newColor :: Tiles -> (Location, Color) -> Color
+newColor m (location, color) = let
+  blackAdjacents = countBlackAdjacents m location
+  in case (color, blackAdjacents) of
+    (Black, 0) -> White
+    (Black, 1) -> Black
+    (Black, 2) -> Black
+    (Black, _) -> White
+    (White, 2) -> Black
+    (White, _) -> White
+
+flipColors :: Tiles -> Tiles
+flipColors m = let
+  newPair (l, c) = (l, newColor m (l, c))
+--  in Map.fromList $ map newPair $ Map.toList $ addAllImplicitAdjacents m
+  in Map.fromAscList $ filter (\(l, c) -> c == Black) $ map newPair $ Map.toAscList $ addAllImplicitAdjacents m
+
+blackAfterNDays :: Tiles -> Int -> Int
+blackAfterNDays m n = countBlackTiles $ (iterate flipColors m)!!n
+
+solvePart2Func :: Int -> [String] -> Int
+solvePart2Func d input = let
+  tiles = map (sumPairs . parseDirections) input
+  part1Map = flipTiles Map.empty tiles
+  in blackAfterNDays part1Map d
+
+solvePart2 :: IO Int
+solvePart2 = let
+  input = readFile "data/input24.txt"
+  in fmap ((solvePart2Func 100) . lines) input
--- 2020/aoc2020/test/Day24Test.hs
+++ 2020/aoc2020/test/Day24Test.hs
@@ -33,16 +33,16 @@ day24PureTests = [ makeTest 10 (solvePart1Func day24TestInput)
                  , makeTest (-3,1) (sumPairs $ parseDirections "nww")
                  , makeTest (-4,0) (sumPairs $ parseDirections "nwwsw")
                  , makeTest (0,0) (sumPairs $ parseDirections "nwwswee")
+                 , makeTest 15 (solvePart2Func 1 day24TestInput)
+                 , makeTest 2208 (solvePart2Func 100 day24TestInput)
                  ]
 
 main = do
   day24Solution1 <- solvePart1
   let day24Solution1Test = testCase [] (assertEqual [] 465
                                         day24Solution1)
-  --day24Solution2 <- solvePart2
-  --let day24Solution2Test =
-  --      testCase [] (
-  --        assertEqual [] "dhfng,pgblcd,xhkdc,ghlzj,dstct,nqbnmzx,ntggc,znrzgs"
-  --          day24Solution2)
+  day24Solution2 <- solvePart2
+  let day24Solution2Test = testCase [] (assertEqual [] 4078
+                                        day24Solution2)
   defaultMain $ testGroup [] (day24PureTests ++
-     [day24Solution1Test]) -- , day24Solution2Test])
+     [day24Solution1Test, day24Solution2Test]) -- , day24Solution2Test])
