e4f56af03b1add32a87bc333f5291b78bd5a0083
mark on github <markongithub@users.noreply.github.com>
2021-12-21 19:57:41 -0500

    solve part 1

--- 2021/aoc2021/src/Day16.hs
+++ 2021/aoc2021/src/Day16.hs
@@ -56,19 +56,58 @@ bitsToDec ls = bitsToDec0 ls 0
 data Packet = Literal Int Int | Operation Int Int [Packet]
   deriving (Eq, Show)
 
-parsePacket :: [Int] -> [Packet]
-parsePacket ls = let
+parsePackets :: [Int] -> Int -> ([Packet], [Int])
+parsePackets [] _ = ([], [])
+parsePackets ls 0 = ([], ls)
+parsePackets ls count = let
   version = bitsToDec $ take 3 ls
   typeID = bitsToDec $ take 3 $ drop 3 ls
   body = drop 6 ls
   (firstPacket, remainder) = case typeID of
     4 -> parseLiteral version body
     _ -> parseOperation version typeID body
-  in firstPacket:(parsePacket remainder)
+  (otherPackets, finalRemainder) = parsePackets remainder (count-1)
+  in (firstPacket:otherPackets, finalRemainder)
+
+parseBitString :: String -> Packet
+parseBitString str = let
+  bits = binStringToBits str
+  in head $ fst $ parsePackets bits 1
+
+parseHex :: String -> Packet
+parseHex str = let
+  bits = hexStringToBits str
+  in head $ fst $ parsePackets bits 1
 
 parseOperation :: Int -> Int -> [Int] -> (Packet, [Int])
 parseOperation version operator body = let
-  version = take 3 body
-  operator = take 3 $ drop 3 body
-  lengthTypeID = head $ drop 6 body
-  in undefined 
+  lengthTypeID = head body
+  countSubpackets = lengthTypeID == 1
+  func = if countSubpackets then parseSubpacketsByCount else parseSubpacketsByBitLength
+  (subpackets, remainder) = func $ tail body
+  in (Operation version operator subpackets, remainder)
+
+parseSubpacketsByCount :: [Int] -> ([Packet], [Int])
+parseSubpacketsByCount body = let
+  count = bitsToDec $ take 11 body
+  in parsePackets (drop 11 body) count
+
+parseSubpacketsByBitLength :: [Int] -> ([Packet], [Int])
+parseSubpacketsByBitLength body = let
+  bitLength = bitsToDec $ take 15 body
+  subPacketsBits = take bitLength $ (drop 15 body)
+  remainder = drop (bitLength + 15) body
+  (subpackets, innerRemainder) = parsePackets subPacketsBits 99
+  in (subpackets, innerRemainder ++ remainder)
+
+versionSum :: Packet -> Int
+versionSum packet = case packet of
+  Literal v _ -> v
+  Operation v _ ps -> v + (sum $ map versionSum ps)
+
+part1Hex :: String -> Int
+part1Hex = versionSum . parseHex
+
+puzzleInput = "A059141803C0008447E897180401F82F1E60D80021D11A3DC3F300470015786935BED80A5DB5002F69B4298A60FE73BE41968F48080328D00427BCD339CC7F431253838CCEFF4A943803D251B924EC283F16D400C9CDB3180213D2D542EC01092D77381A98DA89801D241705C80180960E93469801400F0A6CEA7617318732B08C67DA48C27551C00F972830052800B08550A277416401A5C913D0043D2CD125AC4B1DB50E0802059552912E9676931530046C0141007E3D4698E20008744D89509677DBF5759F38CDC594401093FC67BACDCE66B3C87380553E7127B88ECACAD96D98F8AC9E570C015C00B8E4E33AD33632938CEB4CD8C67890C01083B800E5CBDAB2BDDF65814C01299D7E34842E85801224D52DF9824D52DF981C4630047401400042E144698B2200C4328731CA6F9CBCA5FBB798021259B7B3BBC912803879CD67F6F5F78BB9CD6A77D42F1223005B8037600042E25C158FE0008747E8F50B276116C9A2730046801F29BC854A6BF4C65F64EB58DF77C018009D640086C318870A0C01D88105A0B9803310E2045C8CF3F4E7D7880484D0040001098B51DA0980021F17A3047899585004E79CE4ABD503005E610271ED4018899234B64F64588C0129EEDFD2EFBA75E0084CC659AF3457317069A509B97FB3531003254D080557A00CC8401F8791DA13080391EA39C739EFEE5394920C01098C735D51B004A7A92F6A0953D497B504F200F2BC01792FE9D64BFA739584774847CE26006A801AC05DE180184053E280104049D10111CA006300E962005A801E2007B80182007200792E00420051E400EF980192DC8471E259245100967FF7E6F2CF25DBFA8593108D342939595454802D79550C0068A72F0DC52A7D68003E99C863D5BC7A411EA37C229A86EBBC0CB802B331FDBED13BAB92080310265296AFA1EDE8AA64A0C02C9D49966195609C0594223005B80152977996D69EE7BD9CE4C1803978A7392ACE71DA448914C527FFE140"
+
+solvePart1 = part1Hex puzzleInput
--- 2021/aoc2021/test/Day16Test.hs
+++ 2021/aoc2021/test/Day16Test.hs
@@ -7,7 +7,18 @@ import Day16
 makeTest expected actual = testCase [] (assertEqual [] expected actual)
 
 day16PureTests = [
-    makeTest (Literal 9 2021) (fst $ parseLiteral 9 $ binStringToBits "101111111000101000")
+    makeTest (Literal 9 2021)
+      (fst $ parseLiteral 9 $ binStringToBits "101111111000101000")
+  , makeTest (Literal 6 2021) (parseBitString "110100101111111000101000")
+  , makeTest (Operation 1 6 [Literal 6 10,Literal 2 20])
+      (parseBitString "00111000000000000110111101000101001010010001001000000000")
+  , makeTest (Operation 7 3  [Literal 2 1,Literal 4 2,Literal 1 3])
+      (parseBitString "11101110000000001101010000001100100000100011000001100000")
+  , makeTest 16 (part1Hex "8A004A801A8002F478")
+  , makeTest 12 (part1Hex "620080001611562C8802118E34")
+  , makeTest 23 (part1Hex "C0015000016115A2E0802F182340")
+  , makeTest 31 (part1Hex "A0016C880162017C3686B18A3D4780")
+  , makeTest 860 solvePart1
   ]
 
 day16Tests = day16PureTests
