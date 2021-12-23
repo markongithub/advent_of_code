d26691f46ecbb7e80030e374db6005948c9c9c0b
mark on github <markongithub@users.noreply.github.com>
2021-12-22 21:25:19 -0500

    solve part 1

--- 2021/aoc2021/src/Day21.hs
+++ 2021/aoc2021/src/Day21.hs
@@ -5,6 +5,37 @@ import qualified Data.Map as Map
 import Data.Set (Set)
 import qualified Data.Set as Set
 
-data Node = Start | BigCave String | SmallCave String | End
-  deriving (Eq, Ord, Show)
+data GameState = GameState Int Int Int Int Int Bool
+  deriving (Eq, Show)
 
+applyRollSet :: GameState -> Int -> GameState
+applyRollSet (GameState pos1 pos2 score1 score2 totalRolls isTurn1) rollSum = let
+  addRolls x = (x + rollSum) `mod` 10
+  (newPos1, newPos2) = if isTurn1 then (addRolls pos1, pos2) else (pos1, addRolls pos2)
+  addScore oldScore space = if space == 0 then oldScore + 10 else oldScore + space
+  (newScore1, newScore2) = if isTurn1 then (addScore score1 newPos1, score2) else (score1, addScore score2 newPos2)
+  in GameState newPos1 newPos2 newScore1 newScore2 (totalRolls + 3) (not isTurn1)
+
+hasWinner :: GameState -> Bool
+hasWinner (GameState _ _ score1 score2 _ _) = (score1 >= 1000) || (score2 >= 1000)
+
+applyRollsUntilEnd :: GameState -> [Int] -> GameState
+applyRollsUntilEnd gs [] = gs
+applyRollsUntilEnd gs rs = let
+  (nextRolls, remainder) = splitAt 3 rs
+  nextState = applyRollSet gs (sum nextRolls)
+  in case (hasWinner gs) of
+    True -> gs
+    False -> applyRollsUntilEnd nextState remainder
+
+part1Answer :: Int -> Int -> Int
+part1Answer start1 start2 = let
+  infiniteRolls = concat (repeat [1..100])
+  initialState = GameState start1 start2 0 0 0 True
+  finalState = applyRollsUntilEnd initialState infiniteRolls
+  GameState _ _ score1 score2 totalRolls _ = finalState
+  loserScore = if score1 > score2 then score2 else score1
+  in loserScore * totalRolls
+
+part1Test = part1Answer 4 8
+solvePart1 = part1Answer 8 10 -- my puzzle input
