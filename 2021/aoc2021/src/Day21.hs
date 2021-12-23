09dae0a8df62744953543631456062e5b3aa02a2
mark on github <markongithub@users.noreply.github.com>
2021-12-23 13:28:22 -0500

    Revert "refactor a bit for part 2"
    This reverts commit af203875f540186bd9d5390ad06deccfc6b7f50f.

--- 2021/aoc2021/src/Day21.hs
+++ 2021/aoc2021/src/Day21.hs
@@ -2,6 +2,8 @@ module Day21 where
 
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 data GameState = GameState Int Int Int Int Int Bool
   deriving (Eq, Show)
@@ -14,28 +16,26 @@ applyRollSet (GameState pos1 pos2 score1 score2 totalRolls isTurn1) rollSum = le
   (newScore1, newScore2) = if isTurn1 then (addScore score1 newPos1, score2) else (score1, addScore score2 newPos2)
   in GameState newPos1 newPos2 newScore1 newScore2 (totalRolls + 3) (not isTurn1)
 
-hasWinner :: Int -> GameState -> Bool
-hasWinner threshold (GameState _ _ score1 score2 _ _) = (score1 >= threshold) || (score2 >= threshold)
+hasWinner :: GameState -> Bool
+hasWinner (GameState _ _ score1 score2 _ _) = (score1 >= 1000) || (score2 >= 1000)
 
-applyRollsUntilEnd :: Int -> GameState -> [Int] -> GameState
-applyRollsUntilEnd _ gs [] = gs
-applyRollsUntilEnd threshold gs rs = let
+applyRollsUntilEnd :: GameState -> [Int] -> GameState
+applyRollsUntilEnd gs [] = gs
+applyRollsUntilEnd gs rs = let
   (nextRolls, remainder) = splitAt 3 rs
   nextState = applyRollSet gs (sum nextRolls)
-  in case (hasWinner threshold gs) of
+  in case (hasWinner gs) of
     True -> gs
-    False -> applyRollsUntilEnd threshold nextState remainder
+    False -> applyRollsUntilEnd nextState remainder
 
 part1Answer :: Int -> Int -> Int
 part1Answer start1 start2 = let
   infiniteRolls = concat (repeat [1..100])
   initialState = GameState start1 start2 0 0 0 True
-  finalState = applyRollsUntilEnd 1000 initialState infiniteRolls
+  finalState = applyRollsUntilEnd initialState infiniteRolls
   GameState _ _ score1 score2 totalRolls _ = finalState
   loserScore = if score1 > score2 then score2 else score1
   in loserScore * totalRolls
 
 part1Test = part1Answer 4 8
 solvePart1 = part1Answer 8 10 -- my puzzle input
-
-type UniverseCount = Map GameState Int
