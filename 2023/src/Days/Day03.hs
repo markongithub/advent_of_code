8139b1ceb1edc4c3516844e12f93c12209bf5cf0
mark on github <markongithub@users.noreply.github.com>
2023-12-06 20:14:26 -0500

    this inefficient garbage solves day 3 part 1

--- 2023/src/Days/Day03.hs
+++ 2023/src/Days/Day03.hs
@@ -1,6 +1,7 @@
 module Days.Day03 (runDay) where
 
 {- ORMOLU_DISABLE -}
+import Data.Char (isDigit)
 import Data.List
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
@@ -20,19 +21,71 @@ runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
+parseRow0 :: Int -> String -> Int -> Input -> Input
+parseRow0 _ [] _ accu = accu
+parseRow0 y (c:cs) x (numbers, symbols)
+  | isDigit c = handleDigit
+  | c == '.'  = handleDot
+  | otherwise = handleSymbol
+  where
+    allDigits = c:(Data.List.takeWhile isDigit cs)
+    afterDigits = dropWhile isDigit cs
+    newNumbers = (allDigits, (x, y)):numbers
+    xAfterDigits = x + length allDigits
+    handleDigit = parseRow0 y afterDigits xAfterDigits (newNumbers, symbols)
+    newSymbols = (c, (x,y)):symbols
+    handleSymbol = parseRow0 y cs (x + 1) (numbers, newSymbols)
+    handleDot = parseRow0 y cs (x + 1) (numbers, symbols)
+
+parseRow :: (Int, String) -> Input
+parseRow (y, s) = let
+  initialAccu = ([], [])
+  in parseRow0 y s 0 initialAccu
+
+parseRows :: [String] -> Input
+parseRows ss = let
+  rowsWithIndices = zip [0..] ss
+  parsedRows :: [Input]
+  parsedRows = map parseRow rowsWithIndices
+  initialAccu = ([], [])
+  in foldl mergeInputLines initialAccu parsedRows 
+
+mergeInputLines :: Input -> Input -> Input
+mergeInputLines (xs1, ys1) (xs2, ys2) = (xs1 ++ xs2, ys1 ++ ys2)
+
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = do
+  rowStrs <- many1 (notChar '\n') `sepBy` endOfLine
+  return $ parseRows rowStrs
 
 ------------ TYPES ------------
-type Input = Void
+type Coords = (Int, Int)
+type SchematicNumber = (String, Coords)
+type Symbol = (Char, Coords)
+type Input = ([SchematicNumber], [Symbol])
 
-type OutputA = Void
+type OutputA = Int
 
 type OutputB = Void
 
 ------------ PART A ------------
-partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+
+isNearCoords :: Coords -> SchematicNumber -> Bool
+isNearCoords  (x1, y1) (s, (x2, y2)) = let
+  xRange = [(x2 - 1)..(x2 + length s)]
+  yRange = [y2 - 1, y2, y2+1]
+  allCandidates = [ (x,y) | x<-xRange, y<-yRange ]
+  in elem (x1, y1) allCandidates
+
+isNearSymbol :: [Symbol] -> SchematicNumber -> Bool
+isNearSymbol symbols number = let
+  allCoords = map snd symbols
+  in any (\c -> isNearCoords c number) allCoords
+
+partA :: Input -> Int
+partA (numbers, symbols) = let
+  partNumbers = filter (isNearSymbol symbols) numbers
+  in sum $ map (read . fst) partNumbers
 
 ------------ PART B ------------
 partB :: Input -> OutputB
