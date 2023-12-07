349a06a7eddf33c6b8bf6213007380cca18dc063
mark on github <markongithub@users.noreply.github.com>
2023-12-07 18:59:56 -0500

    Solve day 7, but cleanup is forthcoming

--- 2023/src/Days/Day07.hs
+++ 2023/src/Days/Day07.hs
@@ -1,6 +1,7 @@
 module Days.Day07 (runDay) where
 
 {- ORMOLU_DISABLE -}
+import Data.Char (isDigit, digitToInt)
 import Data.List
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
@@ -20,20 +21,90 @@ runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
+handParser :: Parser Hand
+handParser = do
+  cards <- many1 (notChar ' ')
+  char ' '
+  numbers <- many1 digit
+  return (cards, read numbers)
+
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = handParser `sepBy` endOfLine
 
 ------------ TYPES ------------
-type Input = Void
 
-type OutputA = Void
+type Hand = (String, Int)
 
-type OutputB = Void
+type UniqueCount = Map Char Int
+
+type Input = [Hand]
+
+type OutputA = Int
+
+type OutputB = Int
 
 ------------ PART A ------------
+cardValue :: Bool -> Char -> Int
+cardValue isPart2 c
+  | isDigit c = digitToInt c
+  | otherwise = case c of
+      'A' -> 14
+      'K' -> 13
+      'Q' -> 12
+      'J' -> if isPart2 then 1 else 11
+      'T' -> 10
+      _ -> error (show c ++ " is not a valid card")
+
+breakTie :: Bool -> Hand -> Hand -> Ordering
+breakTie isPart2 (h1, _) (h2, _) = compare (map (cardValue isPart2) h1) (map (cardValue isPart2) h2)
+
+makeUniqueCount :: String -> UniqueCount
+makeUniqueCount cards = let
+  insertOne m c = Map.insertWith (+) c 1 m
+  in foldl insertOne Map.empty cards
+
+handValue :: Hand -> Int
+handValue (cards, _) = let
+  uniqueCount = makeUniqueCount cards
+  frequencies = reverse $ sort $ Map.elems uniqueCount
+  in case frequencies of
+    [5] -> 6
+    [4, 1] -> 5
+    [3, 2] -> 4
+    [3, 1, 1] -> 3
+    [2, 2, 1] -> 2
+    [2, 1, 1, 1] -> 1
+    [1, 1, 1, 1, 1] -> 0
+    _ -> error ("unexpected distribution: " ++ show frequencies)
+
+compareHands :: Bool -> Hand -> Hand -> Ordering
+compareHands isPart2 h1 h2 = let
+  valueFunc = if isPart2 then handValue2 else handValue
+  v1 = valueFunc h1
+  v2 = valueFunc h2
+  in if v1 == v2 then breakTie isPart2 h1 h2 else compare v1 v2
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA hands = let
+  sortedHands = sortBy (compareHands False) hands
+  handsWithRanks = zip [1..] sortedHands
+  winnings (rank, (_, bid)) = rank * bid
+  in sum $ map winnings handsWithRanks
 
 ------------ PART B ------------
+
+handValue2 :: Hand -> Int
+handValue2 (cards, _) = let
+  candidates = filter (/= 'J') $ Set.toList $ Set.fromList cards
+  makeTransformation newC = (map (\oldC -> if oldC == 'J' then newC else oldC) cards, undefined)
+  allTransformations = case cards of
+    "JJJJJ" -> [("JJJJJ", undefined)]
+    _ -> map makeTransformation candidates
+  in maximum $ map handValue allTransformations
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB hands = let
+  sortedHands = sortBy (compareHands True) hands
+  handsWithRanks = zip [1..] sortedHands
+  winnings (rank, (_, bid)) = rank * bid
+  in sum $ map winnings handsWithRanks
