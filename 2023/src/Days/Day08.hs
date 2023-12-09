5b0388c6009f731129aba9d38f331e6efa373bbe
mark on github <markongithub@users.noreply.github.com>
2023-12-08 23:39:00 -0500

    solve part 2

--- 2023/src/Days/Day08.hs
+++ 2023/src/Days/Day08.hs
@@ -74,16 +74,16 @@ routeToZ g source (x:xs) = let
 -- stepsToZ g s dirs = length $ routeToZ g s dirs
 
 stepsToZ :: Graph -> String -> String -> Int -> Int
-stepsToZ g source directions count = stepsToGoal g (== "ZZZ") source directions count
+stepsToZ g source directions count = snd $ stepsToGoal3 g (== "ZZZ") source directions count
 
-funcIndex :: (a -> Bool) -> [a] -> Maybe Int
+funcIndex :: (a -> Bool) -> [a] -> Maybe (a, Int)
 funcIndex f ls = funcIndex0 f ls 0
 
-funcIndex0 :: (a -> Bool) -> [a] -> Int -> Maybe Int
+funcIndex0 :: (a -> Bool) -> [a] -> Int -> Maybe (a, Int)
 funcIndex0 f [] _ = Nothing
-funcIndex0 f (x:xs) count = if f x then Just count else funcIndex0 f xs (count + 1)
+funcIndex0 f (x:xs) count = if f x then Just (x, count) else funcIndex0 f xs (count + 1)
 
-stepsToGoal :: Graph -> (String -> Bool) -> String -> String -> Int -> Int
+stepsToGoal :: Graph -> (String -> Bool) -> String -> String -> Int -> (String, Int)
 stepsToGoal g f source directions count = let
   addStep :: [String] -> Char -> [String]
   addStep ls direction = (followDirection g (head ls) direction ):ls
@@ -91,14 +91,14 @@ stepsToGoal g f source directions count = let
   newCount = count + length directions
   debugMessage = "after " ++ show newCount ++ " steps we are at " ++ show (head allSteps)
   recurse = traceShow debugMessage $ stepsToGoal g f (head allSteps) directions (count + length directions)
-  in case funcIndex f allSteps of
-    Just n -> count + (length directions) - n
+  in case funcIndex f (init allSteps) of
+    Just (winner, n) -> undefined -- (winner, count + (length directions) - n)
     Nothing -> recurse
 
 partA :: Input -> OutputA
 partA (directions, nodes) = let
   graph = makeGraph nodes
-  in stepsToZ graph "AAA" directions 0
+  in stepsToZ graph "AAA" (concat $ repeat directions) 0
 
 ------------ PART B ------------
 
@@ -119,10 +119,44 @@ stepsToZ2 g termFunc sources (dir:ds) count debugPeriod = let
     True -> count
     False -> undefined
 
-partB :: Input -> [(String, Int)]
+stepsToGoal2 :: Graph -> (String -> Bool) -> String -> String -> Int -> (String, Int)
+stepsToGoal2 g f source directions count = let
+  nextSource = followDirection g source (head directions)
+  recurse = stepsToGoal2 g f nextSource (tail directions) (count + 1)
+  in if f source then (source, count) else recurse
+
+stepsToGoal3 :: Graph -> (String -> Bool) -> String -> String -> Int -> (String, Int)
+stepsToGoal3 g f source directions count = let
+  nextSource = followDirection g source (head directions)
+  recurse = stepsToGoal2 g f nextSource (tail directions) (count + 1)
+  in recurse
+
+foldButGenerateList :: (a -> b -> a) -> a -> [b] -> [a]
+foldButGenerateList func start [] = [start]
+foldButGenerateList func start (x:xs) = let
+  recurse = foldButGenerateList func (func start x) xs
+  in (start:recurse)
+
+lookForZLoop :: Graph -> String -> String -> Int -> [(String, Int)]
+lookForZLoop graph source directions count = let
+  infiniteSteps :: [String]
+  infiniteSteps = foldButGenerateList (followDirection graph) source (concat $ repeat directions)
+  numberedSteps :: [(String, Int)]
+  numberedSteps = zip infiniteSteps [0..]
+  filterFunc :: (String, Int) -> Bool
+  filterFunc (s, _) = last s == 'Z'
+  matches = filter filterFunc numberedSteps
+  in Data.List.take count matches
+
+findLoopLength :: Graph -> String -> String -> Int
+findLoopLength graph source directions = snd $ head $ lookForZLoop graph source directions 1
+
+partB :: Input -> Int
 partB (directions, nodes) = let
     graph = makeGraph nodes
     fst3 (x, _, _) = x
     sources = filter (\s -> last s == 'A') $ map fst3 nodes
-    in undefined
---  in stepsToZ2 graph (\s -> last s == 'Z') sources (concat $ repeat directions) 0
+    loopLengths = map (\s -> findLoopLength graph s directions) sources
+    multiple = foldl lcm 1 loopLengths
+    in multiple
+
