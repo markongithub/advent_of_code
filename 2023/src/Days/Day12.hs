503a8b115b4d540169129583bfeeb2497b01b3a5
mark on github <markongithub@users.noreply.github.com>
2023-12-15 06:22:01 -0500

    hell yeah finished day 12

--- 2023/src/Days/Day12.hs
+++ 2023/src/Days/Day12.hs
@@ -64,31 +64,36 @@ canMatchN (x:xs) n = canBeBroken x && canMatchN xs (n-1)
 type Day12Cache = Map (String, [Int]) Int
 
 horribleRecursion :: String -> [Int] -> Int
-horribleRecursion cs ns = fst $ horribleRecursion0 cs ns 0 Map.empty
+horribleRecursion cs ns = fst $ horribleRecursion0 cs ns (length cs) Map.empty
 
 horribleRecursion0 :: String -> [Int] -> Int -> Day12Cache -> (Int, Day12Cache)
 horribleRecursion0 [] [] _ cache = (1, cache)
-horribleRecursion0 (x:xs) [] depth cache = case x of
+horribleRecursion0 (x:xs) [] toGo cache = case x of
   '#' -> (0, cache)
-  _ -> horribleRecursion0 xs [] (depth + 1) cache
+  _ -> horribleRecursion0 xs [] (toGo - 1) cache
 horribleRecursion0 [] (x:xs) _ cache = (0, cache)
-horribleRecursion0 cs0 (n:ns) depth cache = let
-  cs = cs0 -- traceShow ("horribleRecursion0 " ++ cs0 ++ show (n:ns) ++  accu) cs0
+horribleRecursion0 cs0 (n:ns) toGo cache = let
+  cs = cs0 -- if toGo < 100 then cs0 else traceShow ("horribleRecursion0 " ++ cs0 ++ show (n:ns, toGo)) cs0
+  cacheKey = (cs, (n:ns))
   remainderAfterMatch = drop n cs
   shouldRecurseWithMatch = canMatchN cs n
-  (recurseWithMatchNum, cache2) = if shouldRecurseWithMatch then horribleRecursion0 (drop 1 remainderAfterMatch) ns (depth + n + 1) cache else (0, cache)
-  (recurseWithoutMatchNum, cache3) = if mustMatch then (0, cache2) else horribleRecursion0 (tail cs) (n:ns) (depth + 1) cache2
+  (recurseWithMatchNum, cache2) = if shouldRecurseWithMatch then horribleRecursion0 (drop 1 remainderAfterMatch) ns (toGo - (n + 1)) cache else (0, cache)
+  (recurseWithoutMatchNum, cache3) = if mustMatch then (0, cache2) else horribleRecursion0 (tail cs) (n:ns) (toGo - 1) cache2
   mustMatch = (head cs) == '#'
   outputNumber = recurseWithMatchNum + recurseWithoutMatchNum
-  in (outputNumber, cache3)
+  maybeFromCache = if toGo > 20 then Map.lookup cacheKey cache else Nothing
+  finalCache = if toGo > 20 then Map.insert cacheKey outputNumber cache3 else cache3
+  in case maybeFromCache of
+    Nothing -> (outputNumber, finalCache)
+    Just result -> (result, cache) -- traceShow ("cache hit at toGo " ++ show toGo) (result, cache)
 
 arrangements :: (String, [Int]) -> Int
 arrangements (str, nums) = horribleRecursion str nums
 
-partA :: Input -> (OutputA, [Int])
+partA :: Input -> OutputA
 partA input = let
   allValues = map arrangements input
-  in (sum allValues, allValues)
+  in sum allValues
 
 ------------ PART B ------------
 cons :: [a] -> a -> [a]
@@ -102,15 +107,7 @@ expandList s = let
 
 expandInput (str, nums) = (expandList str, concat $ Data.List.take 5 $ repeat nums)
 
-cacheLookup :: (Ord k) => Map k a -> (k -> a) -> k -> (a, Map k a)
-cacheLookup cache func input = let
-  slowResult = func input
-  newCache = Map.insert input slowResult cache
-  in case Map.lookup input cache of
-    Just result -> (result, cache)
-    Nothing -> (slowResult, newCache)
-
--- partB :: Input -> [Int] -- , OutputB)
+-- partB :: Input -> OutputB
 partB input = let
   allValues = map (arrangements . expandInput) input
-  in (3, head input, allValues, sum allValues) -- (allValues, sum allValues)
+  in sum allValues
