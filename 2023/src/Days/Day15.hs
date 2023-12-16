c874ea5a4a24354425dc711007211def4e041c44
mark on github <markongithub@users.noreply.github.com>
2023-12-15 21:08:15 -0500

    finish day 15

--- 2023/src/Days/Day15.hs
+++ 2023/src/Days/Day15.hs
@@ -1,7 +1,7 @@
 module Days.Day15 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.Char (ord)
+import Data.Char (ord, isLetter)
 import Data.List
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
@@ -22,9 +22,7 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = do
-  strs <- many1 (satisfy $ notInClass ",\n") `sepBy` char ','
-  return strs
+inputParser = many1 (satisfy $ notInClass ",\n") `sepBy` char ','
 
 ------------ TYPES ------------
 type Input = [String]
@@ -50,5 +48,53 @@ partA :: Input -> OutputA
 partA input = sum $ map hash input
 
 ------------ PART B ------------
+type Lens = (String, Int)
+type Box = [Lens]
+type Boxes = Map Int Box
+
+addLens :: Box -> Lens -> Box
+addLens [] lens = [lens]
+addLens ((l1, fl1):xs) (l2, fl2) = case l1 == l2 of
+  True -> (l2, fl2):xs
+  False -> ((l1, fl1):(addLens xs (l2, fl2)))
+
+filterOutOne :: (a -> Bool) -> [a] -> [a]
+filterOutOne func [] = []
+filterOutOne func (x:xs) = case func x of
+  True -> xs
+  False -> (x:(filterOutOne func xs))
+
+removeLens :: Box -> String -> Box
+removeLens box label = filterOutOne (\(lab, fl) -> lab == label) box
+
+doCommand :: Boxes -> String -> Boxes
+doCommand boxes str = let
+  label = Data.List.takeWhile isLetter str
+  remainder = drop (length label) str
+  boxNum = hash label
+  box = Map.findWithDefault [] boxNum boxes
+  newBox = case remainder of
+    ('=':fl) -> addLens box (label, read fl)
+    "-" -> removeLens box label
+    _ -> error ("unexpected command: " ++ str)
+  in Map.insert boxNum newBox boxes
+
+lensesInBox :: Box -> [(Lens, Int)]
+lensesInBox box = zip box [1..]
+
+lensesByBox :: Boxes -> [(Lens, Int, Int)]
+lensesByBox boxes = let
+  tuples = Map.toList boxes
+  lensPairToTriple :: Int -> (Lens, Int) -> (Lens, Int, Int)
+  lensPairToTriple boxNum (lens, slot) = (lens, slot, boxNum + 1)
+  listOfTriples :: (Int, Box) -> [(Lens, Int, Int)]
+  listOfTriples (boxNum, box) = map (lensPairToTriple boxNum) $ lensesInBox box
+  in concat $ map listOfTriples tuples
+
+focalPower :: (Lens, Int, Int) -> Int
+focalPower ((_, fl),slotNum,boxNumPlus) = fl * slotNum * boxNumPlus
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB input = let
+  finalBoxes = foldl doCommand Map.empty input
+  in sum $ map focalPower $ lensesByBox finalBoxes
